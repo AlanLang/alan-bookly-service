ca62338a8fcd56a8338da79b1a69303152f2f0dc
Alan <langwdalan@gmail.com>
2018-11-26 13:42:15 +0800

    加入欢迎页

--- app/controller/home.js
+++ app/controller/home.js
@@ -2,7 +2,8 @@ const Controller = require('egg').Controller
 
 class HomeController extends Controller {
   async index() {
-    this.ctx.body = `欢迎使用bookly后台接口服务`
+    this.ctx.redirect('/public/index.html')
+    //this.ctx.body = `欢迎使用bookly后台接口服务`
   }
 }
 
